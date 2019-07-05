f6aa4399a42bdf6cca6387e0a79ad2e0442d1a32
Dora Kemeny <dory1318@gmail.com>
2019-07-05 18:40:55 +0100

    collect the matching urls

--- lib/pageview.rb
+++ lib/pageview.rb
@@ -6,4 +6,10 @@ class PageView
     @collection_of_data = collection_of_data
   end
 
+  def collect_urls
+    url = []
+    self.collection_of_data.each { |urls| url << urls[:url].to_s }
+    collection_of_urls = url.group_by { |urls| urls }
+  end
+
 end
--- spec/pageview_spec.rb
+++ spec/pageview_spec.rb
@@ -3,4 +3,10 @@ require './lib/pageview.rb'
 describe 'PageView' do
    let(:data) { [{:url=>"/home", :ip=>"126.318.035.038"}, {:url=>"/contact", :ip=>"184.123.665.067"}, {:url=>"/home", :ip=>"184.123.665.067"}, {:url=>"/about/2", :ip=>"444.701.448.104"}] }
 
+   it 'collects the url-s returns an array' do
+     sum = PageView.new(data)
+     collection = sum.collect_urls
+     expect(collection).to be_a Hash
+     expect(sum.collect_urls).to eq("/home"=>["/home", "/home"], "/contact"=>["/contact"], "/about/2"=>["/about/2"])
+   end
 end
