6c8651d9768857a024b9adbf29096e3c003b4d57
Dora Kemeny <dory1318@gmail.com>
2019-07-05 18:35:30 +0100

    initialize PageView

---
+++ lib/pageview.rb
@@ -0,0 +1,9 @@
+class PageView
+
+  attr_reader :collection_of_data
+
+  def initialize(collection_of_data)
+    @collection_of_data = collection_of_data
+  end
+
+end
---
+++ spec/pageview_spec.rb
@@ -0,0 +1,6 @@
+require './lib/pageview.rb'
+
+describe 'PageView' do
+   let(:data) { [{:url=>"/home", :ip=>"126.318.035.038"}, {:url=>"/contact", :ip=>"184.123.665.067"}, {:url=>"/home", :ip=>"184.123.665.067"}, {:url=>"/about/2", :ip=>"444.701.448.104"}] }
+
+end
