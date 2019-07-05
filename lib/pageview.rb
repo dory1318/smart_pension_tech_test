d9836de236b26d902cf9fed0ae5ba893ed1858f7
Dora Kemeny <dory1318@gmail.com>
2019-07-05 19:28:07 +0100

    summarizing page views

--- lib/pageview.rb
+++ lib/pageview.rb
@@ -12,4 +12,14 @@ class PageView
     collection_of_urls = url.group_by { |urls| urls }
   end
 
+  def print_summary(page_views)
+    summary = []
+    page_views.each do |k, v|
+      summary << k
+      summary << v.length
+      print "The '#{k}' page had #{v.length} views.\n"
+    end
+    return summary
+  end
+
 end
--- spec/pageview_spec.rb
+++ spec/pageview_spec.rb
@@ -9,4 +9,11 @@ describe 'PageView' do
      expect(collection).to be_a Hash
      expect(sum.collect_urls).to eq("/home"=>["/home", "/home"], "/contact"=>["/contact"], "/about/2"=>["/about/2"])
    end
+
+   it 'prints the summary of page view' do
+     sum = PageView.new(data)
+     collection = sum.collect_urls
+     expect(sum.print_summary(collection)).to eq(["/home", 2, "/contact", 1, "/about/2", 1])
+   end
+
 end
